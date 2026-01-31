2e0130ac3a8bceaa77631c6d842745e3be2171bf
bindujabavurupudi <bindujabavurupudi2006@gmail.com>
2026-01-31 19:38:34 +0530

    created nodemodules and made suabase setup

---
+++ config/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js/dist/index.cjs";
+
+import dotenv from "dotenv"
+
+dotenv.config();
+export const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABSE_KEY
+);
